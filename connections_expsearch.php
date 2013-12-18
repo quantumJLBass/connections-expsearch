3c172a7925c1873de5607065fde09f1e0ce010f3
jeremyBass <jeremybass@cableone.net>
2013-12-18 12:50:31 -0800

    created new shortcode
    used [connections_search] verse [connections] since i need to have a
    form but no listing.

--- connections_expsearch.php
+++ connections_expsearch.php
@@ -21,7 +21,12 @@ if (!class_exists('connectionsExpSearchLoad')) {
 			
 			
 			add_action('cn_action_list_actions', array(__CLASS__, 'showList') );
+			
 			add_action('cn_action_list_before', array(__CLASS__, 'beforeList') );
+			
+			add_shortcode( 'connections_search', array( $this, 'shortcode') );
+			
+			
 		}
 		
 		public static function expand_atts_permitted($permittedAtts){
@@ -41,9 +46,82 @@ if (!class_exists('connectionsExpSearchLoad')) {
 		public static function beforeList($atts){
 			$html = "here at beforeList";//"just your friendly serach form";
 			return $html;
-		}				
+		}
 		
-		
+		/**
+		 * @todo: Add honeypot fields for bots.
+		 */
+		public function shortcode( $atts , $content = NULL ) {
+			global $connections;
+
+			
+
+			$date = new cnDate();
+			$form = new cnFormObjects();
+			$convert = new cnFormatting();
+			$format =& $convert;
+			$entry = new cnEntry();
+			$out = '';
+
+			$atts = shortcode_atts(
+				array(
+					'default_type'     => 'individual',
+					'select_type'      => TRUE,
+					'photo'            => FALSE,
+					'logo'             => FALSE,
+					'address'          => TRUE,
+					'phone'            => TRUE,
+					'email'            => TRUE,
+					'messenger'        => TRUE,
+					'social'           => TRUE,
+					'link'             => TRUE,
+					'anniversary'      => FALSE,
+					'birthday'         => FALSE,
+					'category'         => TRUE,
+					'rte'              => TRUE,
+					'bio'              => TRUE,
+					'notes'            => FALSE,
+					'str_contact_name' => __( 'Entry Name' , 'connections_form' ),
+					'str_bio'          => __( 'Biography' , 'connections_form' ),
+					'str_notes'        => __( 'Notes' , 'connections_form' )
+				), $atts );
+
+
+			/*
+			 * Convert some of the $atts values in the array to boolean.
+			 */
+			$convert->toBoolean($atts['select_type']);
+			$convert->toBoolean($atts['photo']);
+			$convert->toBoolean($atts['logo']);
+			$convert->toBoolean($atts['address']);
+			$convert->toBoolean($atts['phone']);
+			$convert->toBoolean($atts['email']);
+			$convert->toBoolean($atts['messenger']);
+			$convert->toBoolean($atts['social']);
+			$convert->toBoolean($atts['link']);
+			$convert->toBoolean($atts['anniversary']);
+			$convert->toBoolean($atts['birthday']);
+			$convert->toBoolean($atts['category']);
+			$convert->toBoolean($atts['rte']);
+			$convert->toBoolean($atts['bio']);
+			$convert->toBoolean($atts['notes']);
+			//$out .= var_dump($atts);
+
+
+			$out .= '<div id="cn-form-container">' . "\n";
+
+			$out .= '<div id="cn-form-ajax-response"><ul></ul></div>' . "\n";
+
+			$out .= '<form id="cn-form" method="POST" enctype="multipart/form-data">' . "\n";
+
+
+			$out .=  '<p class="cn-add"><input class="cn-button-shell cn-button green" id="cn-form-submit-new" type="submit" name="save" value="' . __('Submit' , 'connections_form' ) . '" /></p>' . "\n";
+
+			$out .= '</form>';
+			$out .= '</div>';
+
+			return $out;
+		}		
 		
 		
 
