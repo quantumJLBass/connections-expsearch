296da5b604711c7619a398446f1b21fef9381568
jeremyBass <jeremybass@cableone.net>
2014-01-10 10:28:11 -0800

    places the stub for optional results display

--- connections_expsearch.php
+++ connections_expsearch.php
@@ -130,28 +130,52 @@ if (!class_exists('connectionsExpSearchLoad')) {
 			
 			//var_dump($categories);
 			//die();
-			if($permittedAtts['category']==NULL){
-
-				$state = isset($_POST['cn-state']) && !empty($_POST['cn-state'])?$_POST['cn-state'].' and ':'';
-				foreach($categories as $cat){
-					$permittedAtts['category']=$cat->term_id;
-					$catblock = connectionsList( $permittedAtts, $content = NULL, $tag = 'connections' );;
-					//var_dump($catblock);
-					if(!empty($catblock) && strpos($catblock,'No results')===false){
-						$out .= '<h3>'.$state.$cat->name.'</h3>';
-						$out .= '<div class="accordion">';
-						$out .= $catblock;
-						$out .= '</div>';
+			
+			$out .= '<div id="tabs" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+				<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+					
+					<li class="ui-state-default ui-corner-top ui-tabs-selected ui-state-active"><a href="#tabs-2">Listings</a></li>
+					<li class="ui-state-default ui-corner-top"><a href="#tabs-1">Map</a></li>
+				</ul>
+			
+				<div id="tabs-2" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
+			';
+			
+			
+			
+				if($permittedAtts['category']==NULL){
+					$state = isset($_POST['cn-state']) && !empty($_POST['cn-state'])?$_POST['cn-state'].' and ':'';
+					foreach($categories as $cat){
+						$permittedAtts['category']=$cat->term_id;
+						$catblock = connectionsList( $permittedAtts, $content = NULL, $tag = 'connections' );;
+						//var_dump($catblock);
+						if(!empty($catblock) && strpos($catblock,'No results')===false){
+							$out .= '<h3>'.$state.$cat->name.'</h3>';
+							$out .= '<div class="accordion">';
+							$out .= $catblock;
+							$out .= '</div>';
+						}
 					}
+				}else{
+					$state = isset($_POST['cn-state']) && !empty($_POST['cn-state'])?$_POST['cn-state'].' and ':'';
+					$category = $connections->retrieve->category($permittedAtts['category']);
+					$out .= '<h3>'.$state.$category->name.'</h3>';
+					$out .= '<div class="accordion">';
+					$out .= connectionsList( $permittedAtts, $content = NULL, $tag = 'connections' );
+					$out .= '</div>';
 				}
-			}else{
-				$state = isset($_POST['cn-state']) && !empty($_POST['cn-state'])?$_POST['cn-state'].' and ':'';
-				$category = $connections->retrieve->category($permittedAtts['category']);
-				$out .= '<h3>'.$state.$category->name.'</h3>';
-				$out .= '<div class="accordion">';
-				$out .= connectionsList( $permittedAtts, $content = NULL, $tag = 'connections' );
-				$out .= '</div>';
-			}
+			
+			$out .='
+			</div>
+			<div id="tabs-1" class="ui-tabs-panel ui-widget-content ui-corner-bottom ">
+				<h2>Hover on a point to find a business and click for more information</h2>
+			<div id="mapJson"></div>
+			<div id="front_cbn_map" class="byState " rel="'.$_POST['cn-state'].'" style="width:100%;height:450px;"></div>
+			<div class="ui-widget-content ui-corner-bottom" style="padding:5px 15px;"><div id="data_display"></div><div style="clear:both;"></div></div>
+			</div>
+		</div>';
+			
+			
 			
 			return $out;
 		}
