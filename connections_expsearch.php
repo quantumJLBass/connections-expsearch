a7d034574669c07ef8cf356593e2fc8268dd7176
jeremyBass <jeremybass@cableone.net>
2014-03-04 12:06:41 -0800

    changes the loader to match partent plugin

--- connections_expsearch.php
+++ connections_expsearch.php
@@ -445,19 +445,38 @@ if (!class_exists('connectionsExpSearchLoad')) {
 
 
 	}
-	
-	/*
-	 * Checks for PHP 5 or greater as required by Connections Pro and display an error message
-	 * rather that havinh PHP thru an error.
+
+
+
+	/**
+	 * Start up the extension.
+	 *
+	 * @access public
+	 * @since 1.0
+	 *
+	 * @return mixed (object)|(bool)
 	 */
-	if (version_compare(PHP_VERSION, '5.0.0', '>')) {
-		/*
-		 * Initiate the plug-in.
-		 */
-		global $connectionsExpSearch;
-		$connectionsExpSearch = new connectionsExpSearchLoad();
-	} else {
-		add_action( 'admin_notices', create_function('', 'echo \'<div id="message" class="error"><p><strong>Connections ROT13 requires at least PHP5. You are using version: ' . PHP_VERSION . '</strong></p></div>\';') );
+	function connectionsExpSearchLoad() {
+
+
+			if ( class_exists('connectionsLoad') ) {
+					return new connectionsExpSearchLoad();
+			} else {
+					add_action(
+							'admin_notices',
+							 create_function(
+									 '',
+									'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Extended Search.</p></div>\';'
+									)
+					);
+					return FALSE;
+			}
 	}
-	
+
+
+	/**
+	 * Since Connections loads at default priority 10, and this extension is dependent on Connections,
+	 * we'll load with priority 11 so we know Connections will be loaded and ready first.
+	 */
+	add_action( 'plugins_loaded', 'connectionsExpSearchLoad', 11 );
 }
