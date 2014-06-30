a270c745e4ef399f9e750fff0748c43d5724a9c7
jeremyBass <jeremybass@cableone.net>
2014-06-30 12:03:48 -0700

    a change to not pick up discounts with no description

--- connections_expsearch.php
+++ connections_expsearch.php
@@ -282,7 +282,7 @@ if (!class_exists('connectionsExpSearchLoad')) {
 			//this is a hard coded for now thing
 			if(isset($_REQUEST['cnbenefits']) && $_REQUEST['cnbenefits']['wsuaa_discounts']){
 				$result = $wpdb->get_results(
-					'SELECT entry_id FROM '.CN_ENTRY_TABLE_META.' WHERE `meta_value` LIKE \'%wsuaa_discounts":1%\'',
+					'SELECT entry_id FROM '.CN_ENTRY_TABLE_META.' WHERE `meta_value` LIKE \'%wsuaa_discounts":1%\' AND `meta_value` NOT LIKE \'%"description":""%\'',
 					ARRAY_A
 				);
 				$wpdb->show_errors();
